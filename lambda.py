1fba213a129948ad755e6e64ca7eddf38480bef0
AndreaCTS <andreats0729@gmail.com>
2024-03-01 02:50:21 +0000

    autoformat

--- lambda.py
+++ lambda.py
@@ -5,22 +5,24 @@ import json
 
 s3 = boto3.client('s3')
 
+
 def lambda_handler(event, context):
-    
+
     # Obtén la fecha actual
     now = datetime.now()
     date_string = now.strftime("%Y-%m-%d")
-    
+
     url = 'https://www.eltiempo.com/'
-    
+
     try:
         # Descargar la página del tiempo
         response = urllib.request.urlopen(url)
         data = response.read()
 
         # Subir el archivo a S3
-        s3.put_object(Body=data, Bucket='zappa-hvwbzmvlm', Key=f'{date_string}.html')
-        
+        s3.put_object(Body=data, Bucket='zappa-hvwbzmvlm',
+                      Key=f'{date_string}.html')
+
         return {
             'statusCode': 200,
             'body': f'Archivo {date_string}.html subido exitosamente a S3'
